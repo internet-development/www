231996e37cdee77eb84e863010b4a249700b7c35
jimmylee <send@jimmy.email>
2024-02-29 23:52:53 -0800

    testimonials update and hanging quote

--- sections/SectionQuote.tsx
+++ sections/SectionQuote.tsx
@@ -6,7 +6,10 @@ export default function SectionQuote(props) {
   return (
     <div className={styles.section}>
       <div className={styles.body}>
-        <p className={styles.heading}>“{props.children}”</p>
+        <p className={styles.heading}>
+          <span className={styles.hanging}>“</span>
+          <span className={styles.remainder}>{props.children}”</span>
+        </p>
       </div>
     </div>
   );
--- sections/SectionTestimonials.tsx
+++ sections/SectionTestimonials.tsx
@@ -2,57 +2,47 @@ import styles from '@sections/SectionTestimonials.module.scss';
 
 import * as React from 'react';
 
+const TestimonialCard = (props) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.space}>
+        <p className={styles.testimonial}>
+          <span className={styles.hanging}>“</span>
+          <span className={styles.remainder}>{props.children}”</span>
+        </p>
+        <div className={styles.author}>
+          <strong>{props.name}</strong>
+          <br />
+          {props.source}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default function SectionTestimonials(props) {
   return (
     <div className={styles.section}>
       <div className={styles.body}>
         <div className={styles.column}>
-          <div className={styles.card}>
-            <div className={styles.space}>
-              <p className={styles.testimonial}>“I don’t know what we would do if we did not have their help.”</p>
-              <div className={styles.author}>
-                <strong>HQ Han</strong>
-                <br />
-                Founder, Ansa Research
-              </div>
-            </div>
-          </div>
+          <TestimonialCard name="HQ Han" source="Founder, Ansa Research">
+            I don’t know what we would do if we did not have their help.
+          </TestimonialCard>
         </div>
         <div className={styles.column}>
-          <div className={styles.card}>
-            <div className={styles.space}>
-              <p className={styles.testimonial}>“Takes initiative, and adapts strategies adeptly for dynamic design projects.”</p>
-              <div className={styles.author}>
-                <strong>Bill Schrekenstein</strong>
-                <br />
-                Engineering Manager, Protocol Labs
-              </div>
-            </div>
-          </div>
+          <TestimonialCard name="Marc & Nima" source="Founders, Nova Energy">
+            Their attention to detail, flexibility, and team dynamics are unparalleled.
+          </TestimonialCard>
         </div>
         <div className={styles.column}>
-          <div className={styles.card}>
-            <div className={styles.space}>
-              <p className={styles.testimonial}>“I feel privileged to have had the opportunity to spend time working together.”</p>
-              <div className={styles.author}>
-                <strong>Dietrich Ayala</strong>
-                <br />
-                Browsers & Platforms, Protocol Labs
-              </div>
-            </div>
-          </div>
+          <TestimonialCard name="Dietrich Ayala" source="Browsers & Platforms, Protocol Labs">
+            I feel privileged to have had the opportunity to spend time working together.
+          </TestimonialCard>
         </div>
         <div className={styles.column}>
-          <div className={styles.card}>
-            <div className={styles.space}>
-              <p className={styles.testimonial}>“Invaluable asset to design teams.”</p>
-              <div className={styles.author}>
-                <strong>Josh Miles</strong>
-                <br />
-                Creative Director, Material
-              </div>
-            </div>
-          </div>
+          <TestimonialCard name="Josh Miles" source="Creative Director, Material">
+            Invaluable asset to design teams.
+          </TestimonialCard>
         </div>
       </div>
     </div>
