133722a4d849e62bd3199dd5682bab63d10f13b2
jimmylee <send@jimmy.email>
2024-06-05 20:28:26 -0700

    what we actually do

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import SectionFooter from '@sections/SectionFooter';
 import SectionNavigation from '@sections/SectionNavigation';
 import SectionProject from '@sections/SectionProject';
 import SectionQuote from '@sections/SectionQuote';
+import SectionSpec from '@sections/SectionSpec';
 
 export async function generateMetadata({ params, searchParams }) {
   const title = 'Internet Development Studio';
@@ -55,29 +56,13 @@ export default async function Page(props) {
         </p>{' '}
         <p className={styles.p}>
           By partnering with INTDEV, you can avoid the process of recruiting an in-house design or development team from scratch. We offer the flexibility to work on projects on a
-          case-by-case basis or embed with you, your team, or your company. Our diverse client portfolio includes venture-backed startups from incubators like Mana Industries,
-          Y-Combinator, and Protocol Labs, as well as organizations like Google.
+          case-by-case basis or embed with you, your team, or your company. Our work history includes venture-backed startups from incubators like Mana Industries, Y-Combinator,
+          and Protocol Labs, as well as companies like Google.
         </p>
         <p className={styles.p}>
-          At INTDEV, we believe in harnessing the power of the design process to create a better internet experience for all. Our design approach combines self-directed learning,
-          understanding complex systems, and embracing simplicity. By continuously seeking knowledge and taking control of our education, we develop a deep understanding of the
-          internet and the world that shapes it. This understanding is essential for creating meaningful, impactful designs that leave lasting results.
+          So what do we actually do? <a href="https://wireframes.internet.dev">(See example work and prototypes)</a>
         </p>
-        <p className={styles.p}>
-          To deliver a great experience on the internet, we recognize the interconnectedness of people surfing the World Wide Web ("WWW") and the importance of creating cohesive
-          and adaptive systems that give individuals engaging with the WWW a sense of place. We strive to strip away the unnecessary and focus on the essential to create designs
-          that are both functional and emotionally resonant. It is not simply about aesthetics or making things pretty; it's about creating experiences that help clarify purpose.
-          We help you be more simple and direct.
-        </p>
-        <p className={styles.p}>
-          Above all else, we are committed to using our skills and knowledge to create a positive impact on the world. We seek to design solutions that helps humanity make
-          progress. Many of our clients work on themes that align with our values, such as combating climate change, fostering collaboration, and reducing the cost of maintaining
-          health. We are proud to contribute to these important causes through our craft.
-        </p>
-        <p className={styles.p}>
-          Thank you for taking the time to learn about INTDEV. We look forward to the opportunity to work together and create meaningful, impactful designs that make a difference
-          in this international space we all share.
-        </p>
+        <SectionSpec />
       </div>
 
       <SectionActions title="Hire us" href="/inquire" cta="Learn more">
---
+++ sections/SectionSpec.tsx
@@ -0,0 +1,112 @@
+import styles from '@sections/SectionSpec.module.scss';
+
+import * as React from 'react';
+
+const Item = (props) => {
+  return (
+    <div className={styles.item}>
+      <div className={styles.left}>
+        <span className={styles.symbol}>☀</span>
+        {props.hide ? null : <span className={styles.line} />}
+      </div>
+      <div className={styles.right}>{props.children}</div>
+    </div>
+  );
+};
+
+const SubItem = (props) => {
+  return (
+    <div className={styles.item} style={{ paddingLeft: 8, opacity: 0.75 }}>
+      <div className={styles.left}>↳</div>
+      <div className={styles.right}>{props.children}</div>
+    </div>
+  );
+};
+
+const SubSubItem = (props) => {
+  return (
+    <div className={styles.item} style={{ paddingLeft: 48, opacity: 0.5 }}>
+      <div className={styles.left}>↳</div>
+      <div className={styles.right}>{props.children}</div>
+    </div>
+  );
+};
+
+export default function SectionSpec(props) {
+  return (
+    <div className={styles.section}>
+      <Item>
+        We join your team
+        <SubItem>Attend standup, meetings, and gatherings</SubItem>
+        <SubItem>Do as much work as we can</SubItem>
+        <SubItem>Be available 24/7</SubItem>
+      </Item>
+      <Item>
+        We make you a new website
+        <SubItem last>Go from zero to a new TypeScript front-end implementation</SubItem>
+        <SubSubItem>Our default tools ➝ NextJS + SASS + Postgres</SubSubItem>
+        <SubSubItem>Our accoutrements ➝ AngularJS, Vue, Svelte</SubSubItem>
+      </Item>
+      <Item>
+        We design you a beautiful interface
+        <SubItem last>Go from zero to as many Figma revisions or pages you need</SubItem>
+        <SubSubItem>Our accoutrements ➝ Sketch, Adobe Photoshop, Adobe Illustrator</SubSubItem>
+      </Item>
+      <Item>
+        We make you a design system
+        <SubItem>Go from zero to a new design system in Figma</SubItem>
+        <SubSubItem>We work on our own.</SubSubItem>
+        <SubSubItem>We researched https://designsystems.surf/design-systems.</SubSubItem>
+        <SubSubItem>We researched https://www.designsystems.com/</SubSubItem>
+        <SubSubItem>We worked with Google Material 3 in production</SubSubItem>
+        <SubItem>Go from zero to a new component library in TypeScript and library of choice</SubItem>
+      </Item>
+      <Item>
+        We help you use your brand
+        <SubItem>Prepare social media templates</SubItem>
+        <SubItem>Manage social media account</SubItem>
+        <SubItem>Design slides and visualize data</SubItem>
+        <SubItem>Implement brand guidelines in code</SubItem>
+      </Item>
+      <Item>
+        We draw you illustrations
+        <SubItem>Design custom characters</SubItem>
+        <SubItem>Prepare marketing spots</SubItem>
+      </Item>
+      <Item>
+        We make you a logo
+        <SubItem>Design an unique logo based on your research</SubItem>
+      </Item>
+      <Item>
+        We build for you
+        <SubItem>Implement a custom content management system</SubItem>
+        <SubItem>Implement a marketing page</SubItem>
+        <SubItem>Implement a new documentation site</SubItem>
+        <SubItem>Implement custom versions of our existing products</SubItem>
+        <SubItem>Go from zero to new product</SubItem>
+        <SubSubItem>We make new products all the time</SubSubItem>
+        <SubSubItem>We specialize in WYSIWYG editors using SlateJS</SubSubItem>
+      </Item>
+      <Item>
+        We build your API
+        <SubItem>Implement an API server for your services</SubItem>
+        <SubSubItem>Our default tools ➝ NodeJS + Postgres</SubSubItem>
+        <SubSubItem>Our accoutrements ➝ Rust implementation, Go implementation</SubSubItem>
+      </Item>
+      <Item>
+        We maintain your deployments
+        <SubItem>Deploy services that support up to a million annual page views</SubItem>
+        <SubItem>Scale horizontally</SubItem>
+        <SubItem>Use Vercel or Render, you decide</SubItem>
+        <SubItem>Manage DNS</SubItem>
+      </Item>
+      <Item hide>
+        We project manage
+        <SubItem>Finish on time</SubItem>
+        <SubItem>Manage issue tracking</SubItem>
+        <SubItem>Provoke discussion</SubItem>
+        <SubItem last>Make accurate estimations</SubItem>
+      </Item>
+    </div>
+  );
+}
